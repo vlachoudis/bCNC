1d708afc35de9d54d884324c14004997c788cc8b
Vasilis Vlachoudis <Vasilis.Vlachoudis@cern.ch>
2016-01-17 13:11:50 +0100

    Incresae minor release number

--- bCNC.py
+++ bCNC.py
@@ -5,8 +5,8 @@
 # Author: vvlachoudis@gmail.com
 # Date: 24-Aug-2014
 
-__version__ = "0.7.5"
-__date__    = "15 Jan 2016"
+__version__ = "0.7.6"
+__date__    = "17 Jan 2016"
 __author__  = "Vasilis Vlachoudis"
 __email__   = "vvlachoudis@gmail.com"
 
