4d1c541288ff8901ad43eaafd457fc306af46d0c
Vasilis <vasilis.vlachoudis@cern.ch>
2018-04-16 15:40:05 +0200

    Added new icon for serial (Issue 784)

--- bCNC.py
+++ bCNC.py
@@ -2043,16 +2043,16 @@ class Application(Toplevel,Sender):
 		if self.serial is not None:
 			self.close()
 			serialPage.connectBtn.config(text=_("Open"),
-						background="LightGreen",
-						activebackground="LightGreen")
+						background="Salmon",
+						activebackground="Salmon")
 		else:
 			serialPage = Page.frames["Serial"]
 			device	 = _device or serialPage.portCombo.get()
 			baudrate = _baud   or serialPage.baudCombo.get()
 			if self.open(device, baudrate):
 				serialPage.connectBtn.config(text=_("Close"),
-							background="Salmon",
-							activebackground="Salmon")
+						background="LightGreen",
+						activebackground="LightGreen")
 				self.enable()
 
 	#-----------------------------------------------------------------------
