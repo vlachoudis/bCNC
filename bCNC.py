6b468a573e18f47d473fb8645a6366ef5456defc
Thomas Buschhardt <github@buschhardt.de>
2018-04-20 21:02:49 +0200

    comment uncomment rows added

--- bCNC.py
+++ bCNC.py
@@ -309,6 +309,7 @@ class Application(Toplevel,Sender):
 		self.bind('<<Enable>>',		self.editor.enable)
 		self.bind('<<Disable>>',	self.editor.disable)
 		self.bind('<<ChangeColor>>',    self.editor.changeColor)
+		self.bind('<<Comment>>',		self.editor.commentRow)
 
 		# Canvas X-bindings
 		self.bind("<<ViewChange>>",	self.viewChange)
