78e2b17be6e21d7eed2b5ff64cf69189d8d8cc52
Tomas Mudrunka <tomas@mudrunka.cz>
2018-09-29 11:45:32 +0200

    Dragknife help #975

--- plugins/dragknife.py
+++ plugins/dragknife.py
@@ -30,15 +30,21 @@ class Tool(Plugin):
 		#Name, Type , Default value, Description
 		self.variables = [			#<<< Define a list of components for the GUI
 			("name"    ,    "db" ,    "", _("Name")),							#used to store plugin settings in the internal database
-			("offset", "mm", 3, _("dragknife offset")),
-			("angle", "float", 20, _("angle threshold")),
-			("swivelz", "mm", 0, _("swivel height")),
-			("initdir", "X+,Y+,Y-,X-,none", "X+", _("initial direction")),
+			("offset", "mm", 3, _("dragknife offset"), _("distance from dragknife rotation center to the tip of the blade")),
+			("angle", "float", 20, _("angle threshold"), _("do not perform pivot action for angles smaller than this")),
+			("swivelz", "mm", 0, _("swivel height"), _("retract to this height for pivots (useful for thick materials, you should enter number slightly lower than material thickness)")),
+			("initdir", "X+,Y+,Y-,X-,none", "X+", _("initial direction"), _("direction that knife blade is facing before and after cut. Eg.: if you set this to X+, then the knifes rotation axis should be on the right side of the tip. Meaning that the knife is ready to cut towards right immediately without pivoting.")),
 			("feed", "mm", 200, _("feedrate")),
-			("simulate", "bool", False, _("simulate")),
-			("simpreci", "mm", 0.5, _("simulation precision"))
+			("simulate", "bool", False, _("simulate"), _("Use this option to simulate cuting of dragknife path. Resulting shape will reflect what shape will actuall be cut. This should reverse the dragknife procedure and give you back the original shape from dragknife g-code.")),
+			("simpreci", "mm", 0.5, _("simulation precision"), _("Simulation is currently approximated by using lots of short lines. This is the length of these lines."))
 		]
 		self.buttons.append("exe")  #<<< This is the button added at bottom to call the execute method below
+
+		self.help = """DragKnifes are special kind of razor/blade holders that can be fit into spindle of your CNC (do not turn the spindle on!!!). They are often used to cut soft and thin materials like vinyl stickers, fabric, leather, rubber gaskets, paper, cardboard, etc...
+
+Dragknife blade is located off center to allow for automatic rotation (kinda like rear wheels of car pivot to the direction of front wheels).
+This fact introduces the need for preprocessing the g-code to account with that offset. Otherwise it wouldn't be able to cut sharp corners. This plugin does this g-code postprocessing.
+"""
 
 
 	# ----------------------------------------------------------------------
@@ -100,7 +106,7 @@ class Tool(Plugin):
 						arcdir = Segment.CCW
 
 					#Append swivel if needed (also always do entry/exit)
-					if abs(angle) > angleth or i == 0 or i == len(opath)-1:
+					if abs(angle) > angleth or (abs(angle) > 1 and ( i == 0 or i == len(opath)-1 )):
 						arca = Segment(arcdir, prevseg.tangentialOffset(dragoff).B, seg.tangentialOffset(dragoff).A, prevseg.B)
 
 						if swivelz !=0: arca._inside = [swivelz]
