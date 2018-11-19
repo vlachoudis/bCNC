7dadb2e731f7b57fa39019789c8ba0a06fb6dfa8
Tomas Mudrunka <tomas@mudrunka.cz>
2018-11-19 01:18:44 +0100

    Added initial setup.py #964

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = "bCNC",
+	version = "0.9.14",
+	description='Swiss army knife for all your CNC/g-code needs',
+	license="GPL",
+	#long_description=long_description,
+	packages = find_packages(),
+	author = "Harvie",
+	#author_email='foomail@foo.com',
+	url="https://github.com/vlachoudis/bCNC",
+	install_requires = [
+		'pyserial==3.4',
+		'numpy==1.15.4',
+		'opencv-python==3.4.2.17',
+	],
+
+	#entry_points = {
+	#	'console_scripts': [
+	#		'bCNC = {package}.{module}:{main_function}',
+	#	]
+	#}
+)
