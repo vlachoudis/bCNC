162446a4de0fc2171d6f5343167e981830f88b3e
Tomas Mudrunka <tomas@mudrunka.cz>
2019-01-08 23:05:10 +0100

    pypi bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
 	name = "bCNC",
-	version = "0.9.14.50",
+	version = "0.9.14.51",
 	license="GPLv2",
 	description='Swiss army knife for all your CNC/g-code needs',
 	long_description=long_description,
