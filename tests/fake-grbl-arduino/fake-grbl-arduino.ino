9042ad8f9937c406eefcf1364874e4ca7b579452
sfinexer <proggi@yandex.ru>
2019-02-20 14:34:30 +0300

    Added send function
    The text "<Idle|MPos:0.000,0.000,0.000|FS:0,0|WCO:0.000,0.000,0.000>\r\n" is too long, having studied the GRBL code, the function printPgmString is used there, I copied it here.
    It is necessary to test better, but the hang of the program at the moment is not observed.

--- tests/fake-grbl-arduino/fake-grbl-arduino.ino
+++ tests/fake-grbl-arduino/fake-grbl-arduino.ino
@@ -1,22 +1,31 @@
 //Fake GRBL for arduino
-
+int8_t readcnt;
+void printPgmString(const char *s)
+{
+    char c;
+    while ((c = pgm_read_byte_near(s++)))
+        Serial.write(c);
+}
 void setup() {
-  Serial.begin(115200);
+    Serial.begin(115200);
 }
 void loop() {}
-
 void serialEvent() 
 {
-  while (Serial.available()) {
-    switch(Serial.read()) {
-      case '\n':
-        Serial.println("ok");
-        break;
-      case '?':
-        Serial.println("<Idle|MPos:0.000,0.000,0.000|FS:0,0|WCO:0.000,0.000,0.000>");
-        Serial.println("ok");
-        break;
+    while (Serial.available()) {
+        memset(buff, 0, sizeof(buff));
+        readcnt = Serial.readBytesUntil(0x0A, buff, sizeof(buff));
+        if (readcnt > 0) 
+        {
+            if ((strncmp(buff, "?", 1) == 0)) {
+                printPgmString("<Idle|MPos:0.000,0.000,0.000|FS:0,0|WCO:0.000,0.000,0.000>\r\n");
+                break;
+            }
+            if ((strncmp(buff, "$G", 2) == 0)) {
+                printPgmString("[GC:G0 G54 G17 G21 G90 G94 M5 M9 T0 F0 S0]\r\n");
+            }
+            printPgmString("ok\r\n");
+        }
+        //delay(5);
     }
-    //delay(5);
-  }
 }
