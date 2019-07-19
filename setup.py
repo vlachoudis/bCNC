5eb7c673b85015ba180c5ea62175e05f96f79e56
Tomas Mudrunka <tomas@mudrunka.cz>
2019-07-19 14:47:59 +0200

    Improved setup.py #1180

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
 	name = "bCNC",
-	version = "0.9.14.58",
+	version = "0.9.14.59",
 	license="GPLv2",
 	description='Swiss army knife for all your CNC/g-code needs',
 	long_description=long_description,
@@ -19,10 +19,11 @@ setup(
 	include_package_data=True,
 	python_requires="<3.0",
 	install_requires = [
-		'pyserial<=3.0.1',	#Windows XP can't handle pyserial newer than 3.0.1
+		"pyserial ; sys_platform != 'win32'",	#Windows XP can't handle pyserial newer than 3.0.1 (it can be installed, but does not work)
+		"pyserial<=3.0.1 ; sys_platform == 'win32'",
 		'numpy>=1.12',
 		'Pillow>=4.0',
-		#'opencv-python>=2.4',	#Note there are no PyPI OpenCV packages for ARM (Raspberry PI, Orange PI, etc...)
+		'opencv-python>=2.4 ; "arm" not in platform_machine',	#Note there are no PyPI OpenCV packages for ARM (Raspberry PI, Orange PI, etc...)
 	],
 
 	entry_points = {
