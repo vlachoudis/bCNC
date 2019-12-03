af90c67ce8065a55094ab2ad77f8eaa40b2194ca
tatarize <tatarize@gmail.com>
2019-12-03 09:13:43 +0000

    Corrected Scale and Type() bugs.

--- bCNC/lib/svgcode.py
+++ bCNC/lib/svgcode.py
@@ -13,7 +13,7 @@
 from __future__ import absolute_import
 
 import numpy
-from svg_elements import SVG, Path, Shape
+from svg_elements import SVG, Path, Shape, Move, Arc, QuadraticBezier, CubicBezier
 
 
 class SVGcode:
@@ -29,31 +29,31 @@ class SVGcode:
 			return ('%*f'%(d,round(v, d))).rstrip("0").rstrip(".")
 
 		for segment in path:
-			subdiv=max(1,round((segment.length(error=1e-5))*subdivratio))
+			subdiv=max(1,round(segment.length(error=1e-5)*subdivratio))
 
-			shape = type(segment).__name__
-			if shape == 'Move':
+			if isinstance(segment, Move):
 				gcode.append('G0 X%s Y%s' % (rv(segment.end.x), rv(-segment.end.y)))
-			elif shape == 'Arc':
+			if isinstance(segment, Arc):
 				if segment.sweep: garc = "G02"
 				else: garc = "G03"
 				#center = segment.center-segment.start
 				#gcode += "(arc %s %s %s %s %s %s)\n"%(segment.center,segment.radius.real,segment.arc,segment.sweep,segment.theta,segment.delta)
 				#garc += ' X%s Y%s I%s J%s\n'%(rv(segment.end.real),rv(-segment.end.imag),rv(center.real),rv(-center.imag))
 				gcode.append('%s X%s Y%s R%s'%(garc, rv(segment.end.x),rv(-segment.end.y),rv(segment.rx)))
-			elif shape in ['QuadraticBezier', 'CubicBezier', 'Arc']:
+			elif isinstance(segment, (QuadraticBezier, CubicBezier, Arc)):
 				subdiv_points = numpy.linspace(0, 1, subdiv, endpoint=False)[1:]
 				for point in subdiv_points:
 					sp = segment.point(point)
 					gcode.append('G1 X%s Y%s'%(rv(sp.x),rv(-sp.y)))
 				gcode.append('G1 X%s Y%s'%(rv(segment.end.x),rv(-segment.end.y)))
 			else:
+				# Line, Close
 				gcode.append('G1 X%s Y%s'%(rv(segment.end.x),rv(-segment.end.y)))
 		return '\n'.join(gcode)
 
 	def get_gcode(self, scale=None, subdivratio=1, digits=4):
 		gcode = []
-		for element in self.svg.elements(ppi=scale, height=10, width=10):
+		for element in self.svg.elements(ppi=96.0 / scale, width=100, height=100):
 			if isinstance(element, Path):
 				element.reify()
 				id = element.id
