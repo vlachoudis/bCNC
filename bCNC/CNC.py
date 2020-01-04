20ff8a409607ecfe987e3a54d5d3c8f65b8a282d
=Pierre Klein <klein.pierre25@gmail.Com>
2020-01-04 13:17:35 +0100

    gCode Time calculation in statistics

--- bCNC/CNC.py
+++ bCNC/CNC.py
@@ -1614,6 +1614,9 @@ class CNC:
 			block.time += length / self.feedmax_x
 			self.totalTime += length / self.feedmax_x
 			block.rapid += length
+		elif self.gcode == 1 or self.gcode==2 or self.gcode==3 :
+			block.time += length / self.feed
+			self.totalTime += length / self.feed
 		else:
 			try:
 				if CNC.vars["feedmode"] == 94:
@@ -1622,7 +1625,6 @@ class CNC:
 				elif CNC.vars["feedmode"] == 93:
 					# Inverse mode
 					t = length * self.feed
-
 				block.time += t
 				self.totalTime += t
 			except:
