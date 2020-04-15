72f6b0289018e8eeebde47c1329e242f289edea1
tsmarks <63697139+tsmarks@users.noreply.github.com>
2020-04-15 00:56:14 -0500

    Fixed tab/space error for Travis CI error

--- bCNC/__main__.py
+++ bCNC/__main__.py
@@ -393,22 +393,22 @@ class Application(Toplevel,Sender):
 			self.bind('<Left>',		self.control.moveYdown)
 			self.bind('<Up>',		self.control.moveXdown)
 			self.bind('<Down>',		self.control.moveXup)
-			self.bind('.',                  self.abccontrol.moveAup)
-                        self.bind(',',                  self.abccontrol.moveAdown)
+			self.bind('.',			self.abccontrol.moveAup)
+			self.bind(',',			self.abccontrol.moveAdown)
 		elif self._swapKeyboard == -1:
 			self.bind('<Right>',		self.control.moveYdown)
 			self.bind('<Left>',		self.control.moveYup)
 			self.bind('<Up>',		self.control.moveXup)
 			self.bind('<Down>',		self.control.moveXdown)
 			self.bind(',',                  self.abccontrol.moveAup)
-                        self.bind('.',                  self.abccontrol.moveAdown)
+			self.bind('.',			self.abccontrol.moveAdown)
 		else:
 			self.bind('<Right>',		self.control.moveXup)
 			self.bind('<Left>',		self.control.moveXdown)
 			self.bind('<Up>',		self.control.moveYup)
 			self.bind('<Down>',		self.control.moveYdown)
 			self.bind('.',                  self.abccontrol.moveAup)
-                        self.bind(',',                  self.abccontrol.moveAdown)
+			self.bind(',',			self.abccontrol.moveAdown)
 
 		try:
 			self.bind('<KP_Prior>',		self.control.moveZup)
